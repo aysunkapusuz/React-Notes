9b2d02ba58b624e6ef0c07f439d23e008733edd4
Aysun <aysunkapusuz@gmail.com>
2023-02-06 10:40:52 -0700

    first commit

---
+++ whatIsReact.js
@@ -0,0 +1,30 @@
+// What is React & Core Principles
+
+// "A JS library(some pre-written code) for building user interfaces"
+
+// 3 Principles of React via their docs
+
+
+// 1)Declarative 
+
+// You tell React what you want your page to look like
+// what HTML it should render, and React will figure out // React will use JS to implement what you want it to look like
+// How to create and render that HTML in the most optimal way
+// This improves predictability and is easier to debug
+
+// 2)Component-Based
+
+// Build individual components (they are functions) that create part of your User Interface
+// Logic is written in JS
+// Re-use these components anywhere within your app
+// Allows you break down complex apps into easy to create/use components
+
+// 3) Learn Once, Write Anywhere
+
+// React also works for mobile apps, on the server, and can be added to an existing teck stack
+
+//React Features 
+//JSX(It's a syntax looks like HTML but it is actually JS), Virtual DOM improving performance, one-way data flow(data can only flow from parent to child), strong community
+
+//What to know before React
+// HTML, CSS, JS
