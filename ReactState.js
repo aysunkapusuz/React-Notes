64c0f825eb93496c4178776f726cc213402ccd50
Aysun <aysunkapusuz@gmail.com>
2023-02-10 08:51:38 -0700

    react state

---
+++ ReactState.js
@@ -0,0 +1,10 @@
+// 1)What is state in React?
+
+// The data of an app at a given point in time
+// This data will lead to what the user will see on their page
+// State can change over time. So, the "state" of your app may vary
+//For data(state) that changes over time, React gives you some ways for handling that and one way is with the useState function
+
+//// You can use useState() hook to manage state within a React component
+// Hooks must be called at the top of the component
+// Hooks can not be called inside loops, conditions, nested functions.
