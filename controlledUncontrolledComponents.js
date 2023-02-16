e01f5e2e0853a922e5505aa31ecfd6aa14026def
Aysun <aysunkapusuz@gmail.com>
2023-02-16 08:38:34 -0700

    react controlled and uncontrolled components

---
+++ controlledUncontrolledComponents.js
@@ -0,0 +1,14 @@
+// What does something to be controlled mean in React?
+
+// Controlled and uncontrolled are used to describe whether you're managing the values  within your elements, or if you're just letting the browser handle it with its default behavior
+
+// A controlled component is where the developer, manages the vaues within your HTML elements
+
+// An uncontrolled component is where you allow the browser to manage the values within elements
+
+// What are the benefits of using controlled?
+
+// Instant validation of inputs
+// Enforce formats
+//Makes React state the single source of truth
+//Gives you more flexibility when you use input values
